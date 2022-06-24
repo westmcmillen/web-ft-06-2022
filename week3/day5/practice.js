a52074991d7820d69d24267cbffcc3de655b2b6d
West <westmcmillen@gmail.com>
2022-06-24 13:27:44 -0700

    Create practice.js

---
+++ week3/day5/practice.js
@@ -0,0 +1,10 @@
+console.log( "Hello" );
+
+const restaurants = [ "Carmelita's", "Jose's", "Municipal Taco" ];
+
+// console.log( restaurants[ restaurants.length - 1 ] );
+// console.log( restaurants.at( -1 ) );
+
+for ( let restaurant of restaurants ) {
+    console.log( restaurant );
+}
