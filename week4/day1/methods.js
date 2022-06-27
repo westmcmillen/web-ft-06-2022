b9b9c77e622d046d866b4c0ad9ee0c936434c6b4
West <westmcmillen@gmail.com>
2022-06-27 11:17:31 -0700

    Create methods.js

---
+++ week4/day1/methods.js
@@ -0,0 +1,40 @@
+function lvl5exercise1() {
+    // Push the string "hello" into the array and return the array
+    var arr = [1, "adam"];
+}
+
+function lvl5exercise2() {
+    // Remove the last element from the array and return the array
+    var arr = [1, "adam"];
+}
+
+function lvl5exercise3() {
+    // Return the length of the array
+    var arr = [1, "adam"];
+}
+
+function lvl5exercise4() {
+    // Return the index of item "adam" in the array
+    var arr = [1, "adam"];
+}
+
+function lvl6exercise1(num) {
+    // Return 'hello' if num is 1, 'world' if num is 2, otherwise return 'bye'
+}
+
+function lvl6exercise2() {
+    // Push 10 'hello' strings into the array using a for loop, then return it
+    var arr = [];
+}
+
+function lvl6exercise3() {
+    // Empty this array using a while loop and return it
+    var arr = ["hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello"];
+}
+
+// Write a function that takes a number as an input.
+// Have it create an empty array and then push a string into the array as many
+// times as the input number. If the input is anything other than a positive
+// integer return an empty array
+//
+// Name the function "finalFunction"
