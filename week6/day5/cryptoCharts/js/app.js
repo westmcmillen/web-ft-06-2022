5855bb1b91fe439b1b211a4604b5c2034ad09c93
West <76145386+westmcmillen@users.noreply.github.com>
2022-07-12 10:41:37 -0700

    commit

--- week6/day5/cryptoCharts/js/app.js
+++ week6/day5/cryptoCharts/js/app.js
@@ -1,3 +1,5 @@
+const API_KEY = config.API_KEY;
+
 const appNav = document.getElementById("app-nav");
 const hamburger = document.getElementById("hamburger");
 
@@ -52,7 +54,65 @@ const addTab = () => {
     clearTabInputValue();
 };
 
+// Chart
+
+const plotChart = async ticker => {
+    const response = await fetch(`https://www.alphavantage.co/query?function=CRYPTO_INTRADAY&symbol=${ticker}&market=USD&interval=1min&apikey=${API_KEY}`);
+    const json = await response.json();
+    const timeSeries = json["Time Series Crypto (1min)"];
+    const ohlc = Object.values(timeSeries);
+
+    const periodLow = Math.min(...ohlc.map(value => value["3. low"]));
+    const periodHigh = Math.max(...ohlc.map(value => value["2. high"]));
+    const periodRange = Math.ceil(periodHigh - periodLow);
+
+    const chart = document.createElement("figure");
+    chart.className = "chart";
+
+    chart.style.gridTemplateColumns = `repeat(${ohlc.length * 4}, minmax(0, 1fr))`;
+
+    let column = 1;
+
+    chart.innerHTML = null;
+
+    ohlc.reverse().map(value => {
+        value["1. open"] = Math.round((value["1. open"] - periodLow) / periodRange / 0.01);
+        value["2. high"] = Math.round((value["2. high"] - periodLow) / periodRange / 0.01);
+        value["3. low"] = Math.round((value["3. low"] - periodLow) / periodRange / 0.01);
+        value["4. close"] = Math.round((value["4. close"] - periodLow) / periodRange / 0.01);
+        console.log(value["1. open"], value["2. high"], value["3. low"], value["4. close"]);
+
+        const candlestick = document.createElement("div");
+        candlestick.className = "candlestick";
+        candlestick.style.gridRow = `${value["1. open"]} / ${value["4. close"]}`;
+        candlestick.style.gridColumn = `${column} / ${column + 3}`;
+
+        const wick = document.createElement("div");
+        wick.className = "wick";
+        wick.style.gridRow = `${value["2. high"]} / ${value["3. low"]}`;
+        wick.style.gridColumn = `${column + 1} / ${column + 2}`;
+
+        if (value["1. open"] < value["4. close"]) {
+            candlestick.style.backgroundImage = "linear-gradient(crimson, red)";
+        }
+
+        if (value["1. open"] > value["4. close"]) {
+            candlestick.style.backgroundImage = "linear-gradient(lightgreen, seagreen)";
+        }
+
+        if (value["1. open"] === value["4. close"]) {
+            candlestick.style.backgroundImage = "linear-gradient(silver, grey)";
+        }
+        chart.append(wick);
+        chart.append(candlestick);
+        column = column + 4;
+    });
+};
+
+// chart end
+
 tabForm.onsubmit = event => {
     event.preventDefault();
+    plotChart(getTabInputValue());
     addTab();
 };
