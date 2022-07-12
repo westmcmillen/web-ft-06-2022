f78cabf3f17fa3cf3c6b6733c1a1b9b2b979d83f
West <76145386+westmcmillen@users.noreply.github.com>
2022-07-12 12:18:09 -0700

    commit

--- week6/day5/cryptoCharts/js/app.js
+++ week6/day5/cryptoCharts/js/app.js
@@ -55,6 +55,7 @@ const addTab = () => {
 };
 
 // Chart
+const chartMount = document.getElementById("chart-mount");
 
 const plotChart = async ticker => {
     const response = await fetch(`https://www.alphavantage.co/query?function=CRYPTO_INTRADAY&symbol=${ticker}&market=USD&interval=1min&apikey=${API_KEY}`);
@@ -67,36 +68,36 @@ const plotChart = async ticker => {
     const periodRange = Math.ceil(periodHigh - periodLow);
 
     const chart = document.createElement("figure");
+    chart.id = getTabInputValue();
     chart.className = "chart";
 
     chart.style.gridTemplateColumns = `repeat(${ohlc.length * 4}, minmax(0, 1fr))`;
 
-    let column = 1;
+    let columnIndex = 1;
 
     chart.innerHTML = null;
 
-    ohlc.reverse().map(value => {
+    ohlc.map(value => {
         value["1. open"] = Math.round((value["1. open"] - periodLow) / periodRange / 0.01);
         value["2. high"] = Math.round((value["2. high"] - periodLow) / periodRange / 0.01);
         value["3. low"] = Math.round((value["3. low"] - periodLow) / periodRange / 0.01);
         value["4. close"] = Math.round((value["4. close"] - periodLow) / periodRange / 0.01);
-        console.log(value["1. open"], value["2. high"], value["3. low"], value["4. close"]);
 
         const candlestick = document.createElement("div");
         candlestick.className = "candlestick";
         candlestick.style.gridRow = `${value["1. open"]} / ${value["4. close"]}`;
-        candlestick.style.gridColumn = `${column} / ${column + 3}`;
+        candlestick.style.gridColumn = `${columnIndex} / ${columnIndex + 3}`;
 
         const wick = document.createElement("div");
         wick.className = "wick";
         wick.style.gridRow = `${value["2. high"]} / ${value["3. low"]}`;
-        wick.style.gridColumn = `${column + 1} / ${column + 2}`;
+        wick.style.gridColumn = `${columnIndex + 1} / ${columnIndex + 2}`;
 
-        if (value["1. open"] < value["4. close"]) {
+        if (value["1. open"] > value["4. close"]) {
             candlestick.style.backgroundImage = "linear-gradient(crimson, red)";
         }
 
-        if (value["1. open"] > value["4. close"]) {
+        if (value["1. open"] < value["4. close"]) {
             candlestick.style.backgroundImage = "linear-gradient(lightgreen, seagreen)";
         }
 
@@ -105,8 +106,9 @@ const plotChart = async ticker => {
         }
         chart.append(wick);
         chart.append(candlestick);
-        column = column + 4;
+        columnIndex = columnIndex + 4;
     });
+    chartMount.append(chart);
 };
 
 // chart end
