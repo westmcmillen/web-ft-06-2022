dbb19a40ce653fd91cbf44460307bf05d535403d
West <76145386+westmcmillen@users.noreply.github.com>
2022-07-22 06:15:41 -1000

    Update order.js

--- week7/day5/example1/database/models/order.js
+++ week7/day5/example1/database/models/order.js
@@ -1,25 +1,21 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Order extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       // define association here
     }
   }
-  Order.init({
-    userId: DataTypes.STRING,
-    longboardId: DataTypes.STRING,
-    price: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Order',
-  });
+  Order.init(
+    {
+      userId: DataTypes.STRING,
+      longboardId: DataTypes.STRING,
+      price: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "Order",
+    },
+  );
   return Order;
 };
