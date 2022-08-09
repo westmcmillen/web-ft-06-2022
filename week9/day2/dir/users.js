5dc3a633aadffa093d8356c36f805ee744ce5a24
West <76145386+westmcmillen@users.noreply.github.com>
2022-08-09 05:25:09 -1000

    compiled users.js

--- week9/day2/dir/users.js
+++ week9/day2/dir/users.js
@@ -1,1 +1,20 @@
 "use strict";
+const stacy = {
+  name: "Stacy",
+  chatty: true,
+  cohort: "Full Time",
+  instructor: "Joe",
+  employed: false,
+  ta: "Violet",
+};
+const Amanda = {
+  name: "Amanda",
+  chatty: true,
+  cohort: "Full Time",
+  instructor: "Joe",
+  employed: false,
+  ta: "Violet",
+  typingSpeed: 9000,
+  jobOffers: "Google",
+  algoProficiency: "master",
+};
